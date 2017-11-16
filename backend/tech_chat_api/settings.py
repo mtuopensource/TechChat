388891ddbf075660d5a1821ecf1d762d19ce22c7
Austin Walhof <ajwgeek@gmail.com>
2017-11-15 23:29:31 -0500

    Fixed tabs in settings.py

--- backend/tech_chat_api/settings.py
+++ backend/tech_chat_api/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'rest_framework',
+    'rest_framework',
     'rest_framework_mongoengine',
-	'api',
+    'api',
 ]
 
 MIDDLEWARE = [
