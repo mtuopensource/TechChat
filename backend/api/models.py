120a29bb6a62da87494d3f174d04c639c59a0dbc
Austin Walhof <ajwgeek@gmail.com>
2017-11-28 19:34:03 -0500

    Added link to mongo field documentation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
 from mongoengine import Document, EmbeddedDocument, fields
 from mongoengine.queryset.base import DO_NOTHING
 
+#http://docs.mongoengine.org/guide/defining-documents.html
+
 class Board(Document):
     title = fields.StringField(max_length=32, required=True)
     description = fields.StringField(max_length=128, required=True, null=True)
