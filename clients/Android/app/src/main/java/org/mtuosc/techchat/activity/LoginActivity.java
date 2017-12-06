8e8a3b53ea9c1c6890eca20c7ba4a5baedd47e8e
Ryaguy15 <raphilip.mtu.edu>
2017-12-05 20:06:15 -0500

    Created the Login activity. Made a nice login screen the user is greeted to.
    Signed-off-by: Ryaguy15 <raphilip.mtu.edu>

---
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/activity/LoginActivity.java
@@ -0,0 +1,60 @@
+package org.mtuosc.techchat.activity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mtuosc.techchat.R;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Moves the user to the create user activity
+     * @param view this should be the text view
+     */
+    public void moveToUserCreation(View view) {
+        Toast.makeText(this, "Moving to Create User", Toast.LENGTH_SHORT).show();
+    }
+}
+
