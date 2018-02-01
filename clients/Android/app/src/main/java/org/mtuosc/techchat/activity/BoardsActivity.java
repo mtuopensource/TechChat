f5e05d311403901e62fbfbe2a2fc2770428814de
Ryan Philipps <raphilip@mtu.edu>
2018-02-01 13:50:47 -0500

    Edited the Signup layout

--- clients/Android/app/src/main/java/org/mtuosc/techchat/activity/BoardsActivity.java
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/activity/BoardsActivity.java
@@ -39,20 +39,18 @@ public class BoardsActivity extends AppCompatActivity implements NavigationView.
     private RecyclerView boardRecyclerView;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_activity_base);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
--- clients/Android/app/src/main/java/org/mtuosc/techchat/activity/CreateUserActivity.java
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/activity/CreateUserActivity.java
@@ -3,6 +3,9 @@ package org.mtuosc.techchat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import org.mtuosc.techchat.R;
 
@@ -15,5 +18,13 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_user_layout);
+        TextView termsOfServ = createTermsOfServiceText();
+    }
+    private TextView createTermsOfServiceText(){
+        TextView termsOfServ = findViewById(R.id.terms_of_service);
+        termsOfServ.setText(Html.fromHtml("<p> By Tapping the button above, you subscribe yourself to our " +
+                "<a href=\"http://google.com\">Terms of Service</a> and our <a href=\"https://google.com\">Privacy Policy</a> </p>"));
+        termsOfServ.setMovementMethod(LinkMovementMethod.getInstance());
+        return termsOfServ;
     }
 }
