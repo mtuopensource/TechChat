f27ed8b1497f587c88cd152ffa8ad12d70f023d6
Austin Walhof <ajwgeek@gmail.com>
2018-02-02 20:49:59 -0500

    Removed unused permission class

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,14 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from api.models import User
 
-# Authenticated have read access. Staff members have write access.
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return request.user and request.user.is_authenticated
-        else:
-            return request.user and request.user.is_staff
-
 class TechChatIsAuthenticated(BasePermission):
     def has_permission(self, request, view):
         return 'techchat_userid' in request.session
