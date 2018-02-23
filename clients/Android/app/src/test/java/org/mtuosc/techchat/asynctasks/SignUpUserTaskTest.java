7b82b49ed0454914e385047d0f71c6da422d80fb
Ryan Philipps <raphilip@mtu.edu>
2018-02-23 11:48:00 -0500

    Fixed some android test. Created a test for signup
    Signed-off-by: Ryan Philipps <raphilip@mtu.edu>

--- clients/Android/app/src/test/java/org/mtuosc/techchat/asynctasks/SignUpUserTaskTest.java
+++ clients/Android/app/src/test/java/org/mtuosc/techchat/asynctasks/SignUpUserTaskTest.java
@@ -1,5 +1,12 @@
 package org.mtuosc.techchat.asynctasks;
 
+import com.goebl.david.Response;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,5 +14,26 @@ import static org.junit.Assert.*;
  */
 public class SignUpUserTaskTest {
 
+
+    @Test
+    public void testUserCreation(){
+
+        SignUpUserTask task = new SignUpUserTask("http://ci.mtuopensource.club:8000",
+            "tester@mtu.edu","test",new AsyncApiResponse<Response<JSONObject>>() {
+                @Override
+                public void taskCompleted(Response<JSONObject> result) {
+                      if (result.getStatusCode() != 201)
+                          fail();
+                }
+            });
+
+            task.execute();
+        try {
+            Thread.sleep(200); // giving time for server to run
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
 
 }
