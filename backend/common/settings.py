4bc4310243c21cd72dc8b6a036d6121613bcd121
Austin Walhof <ajwgeek@gmail.com>
2018-02-23 00:47:42 -0500

    Register web app with common project

--- backend/common/settings.py
+++ backend/common/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_mongoengine',
     'api',
+    'web'
 ]
 
 MIDDLEWARE = [
--- backend/common/urls.py
+++ backend/common/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('api.urls'))
+    url(r'^api/', include('api.urls')),
+    url(r'^web/', include('web.urls'))
 ]
