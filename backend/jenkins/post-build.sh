df41bc5bf6f1c349011980e5555c8c76d6f8e3fb
Austin Walhof <ajwgeek@gmail.com>
2018-03-22 21:45:59 -0400

    Update build scripts

--- backend/jenkins/post-build.sh
+++ backend/jenkins/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$WORKSPACE/backend"
 pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
+python3.6 manage.py migrate
+python3.6 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
