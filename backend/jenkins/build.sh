5076c064fb6f3340f446dc1aeac3e251b8502c2a
Austin Walhof <ajwgeek@gmail.com>
2018-09-18 02:41:18 -0400

    Removed jenkins config

--- backend/jenkins/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-killall -q python3.6 || true
-cd "$WORKSPACE/backend"
-pip3 install --user -r requirements.txt
-rm .env || true
-echo "HOST=mongodb://user:pass@localhost/db?authSource=authDb" >> .env
-echo "ENCODING=utf-8" >> .env
-python3.6 manage.py migrate
-python3.6 manage.py test
