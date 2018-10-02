bcb7ab873459ee33d09b21b88c5465558a8e5c47
Austin Walhof <ajwgeek@gmail.com>
2018-10-02 00:41:57 -0400

    Remove experimental web client

--- backend/web/static/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  window.mdc.autoInit();
-
-  /* Update the hidden input when the form submits. */
-  $('#create-thread-form').submit(function() {
-    let select = new mdc.select.MDCSelect($('#board-select')[0]);
-    $('input[name=board]').val(select.value);
-  });
-});
--- backend/web/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from web.views.IndexView import index, board, thread, createthread, login, logout
-
-__all__ = ['index', 'board', 'thread', 'createthread', 'login', 'logout']
