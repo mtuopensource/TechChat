c5fa8713a5b5e9f726951a4dcb129d150ea0d860
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 14:23:41 -0400

    Create views package

---
+++ backend/api/views/__init__.py
@@ -0,0 +1,5 @@
+from api.viewsets.CreateRetrieveUpdateDestroy import CreateRetrieveUpdateDestroy
+from api.viewsets.BoardViewSet import BoardViewSet
+from api.viewsets.PostViewSet import PostViewSet
+from api.viewsets.CommentViewSet import CommentViewSet
+
