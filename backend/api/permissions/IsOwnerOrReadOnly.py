74e526c296d8d106ece3365e5f39260838eedfb1
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 17:08:01 -0400

    Add documentation to IsOwnerOrReadOnly

--- backend/api/permissions/IsOwnerOrReadOnly.py
+++ backend/api/permissions/IsOwnerOrReadOnly.py
@@ -3,6 +3,18 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, instance):
+        """
+        Determines if the User(s) have permission to perform a destructive action.
+        User(s) considered an 'owner' of an object are given this permission.
+
+        Parameters:
+            request: HttpRequest containing the method and user.
+            view: Unused.
+            instance: Object with an 'owner' attribute.
+
+        Returns:
+            True if the User(s) have permission. False otherwise.
+        """
         if request.method in SAFE_METHODS:
             return True
         return instance.owner == request.user
