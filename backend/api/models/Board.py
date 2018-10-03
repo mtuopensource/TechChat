1e4a19b73dc8e7113f09e689627afccef2cc88b2
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 15:20:06 -0400

    Added documentation to Board

--- backend/api/models/Board.py
+++ backend/api/models/Board.py
@@ -2,6 +2,15 @@ from django.db.models import Model, CharField, SlugField
 
 
 class Board(Model):
+    """
+    Represents a forum, dedicated to a specific topic.
+
+    Attributes:
+        title: The Board's name. Limited to 32 characters. Must be unique.
+        description: Short description of the Board's intended use. Limited to 128 characters.
+        slug: Reserved for the web application. Limited to 32 characters. Must be unique.
+    """
+
     title = CharField(max_length=32, unique=True)
     description = CharField(max_length=128)
     slug = SlugField(max_length=32, unique=True)
