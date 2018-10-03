1c651ce9bd7ec8b27bbc684d6ff20eaeb271408f
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 16:04:07 -0400

    Add documentation to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.admin import register, ModelAdmin
 from api.models import Board, Post, Comment
 
+# Register Board, Post, and Comment with Django's CMS.
 register(Board, Post, Comment)(ModelAdmin)
