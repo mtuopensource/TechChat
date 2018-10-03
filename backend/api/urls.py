b8fb02b7edcd7c1706a4f277d25d429bf7f8f9eb
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 18:00:05 -0400

    Cleanup imports in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,12 @@
+from api.views import BoardViewSet, PostViewSet, CommentViewSet
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-
-from api.views.BoardViewSet import BoardViewSet
-from api.views.PostViewSet import PostViewSet
-from api.views.CommentViewSet import CommentViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
-router.register(r'boards', BoardViewSet, r'board')
-router.register(r'posts', PostViewSet, r'post')
-router.register(r'comments', CommentViewSet, r'comment')
+router.register(r'comments', CommentViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'boards', BoardViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
