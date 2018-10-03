309af13c20b2234d631673b7baeb6e421440ba12
Austin Walhof <ajwgeek@gmail.com>
2018-10-03 15:41:48 -0400

    Added documentation to Post

--- backend/api/models/Post.py
+++ backend/api/models/Post.py
@@ -4,12 +4,25 @@ from api.models.Board import Board
 
 
 class Post(Model):
+    """
+    Represents a discussion within a Board.
+    Consists of a user-submitted message containing the User's details and the date and time it was submitted.
+
+    Attributes:
+        title: The Post's title. Limited to 32 characters.
+        description: Summary of the intended discussion. Limited to 512 characters.
+        author: UUID of the Post's author.
+        ip: IPv4 or IPv6 address that originated the Post. Normalized following RFC 4291.
+        timestamp: The Post's publication time. See RFC 3339.
+        board: UUID of the Post's discussion Board.
+    """
+
+    title = CharField(max_length=32)
+    description = TextField(max_length=512)
     author = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
     ip = GenericIPAddressField()
     timestamp = DateTimeField(auto_now_add=True)
     board = ForeignKey(Board, on_delete=CASCADE)
-    title = CharField(max_length=32)
-    content = TextField(max_length=512)
 
     @property
     def owner(self):
