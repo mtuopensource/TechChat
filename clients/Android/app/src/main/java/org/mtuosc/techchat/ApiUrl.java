546547b19013db9a941faf2ba4ae65d13bf40c2c
Ryan Philipps <raphilip@mtu.edu>
2018-10-23 18:24:22 -0400

    Fixed the login to use the api calls

--- clients/Android/app/src/main/java/org/mtuosc/techchat/ApiUrl.java
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/ApiUrl.java
@@ -10,7 +10,9 @@ public class ApiUrl {
     public static final String SERVER_HOSTNAME = "open-source-at-mtu-tech-chat.herokuapp.com";
     public static final String SERVER_URL = "https://open-source-at-mtu-tech-chat.herokuapp.com";
     public static final String GET_BOARDS = "/api/boards/";
-    public static final String LOGIN = "/api/users/login/";
+    public static final String LOGIN = "/api/token/";
+    public static final String REFRESH = "/api/token/refresh/";
+    public static final String VERIFY = "/api/token/verify/";
     public static final String SIGN_UP = "/api/users/";
 
 
