ee84cd7ee68465741f753db5d78629e291b5b106
Ryan Philipps <raphilip@mtu.edu>
2019-01-04 18:07:46 -0500

    Created a rough template for the post layout

--- clients/Android/app/src/main/java/org/mtuosc/techchat/activity/PostActivity.java
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/activity/PostActivity.java
@@ -1,24 +1,26 @@
 package org.mtuosc.techchat.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.mtuosc.techchat.R;
 
-public class ThreadsActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.thread_activity_main);
+        setContentView(R.layout.post_activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Posts for Board");
         setSupportActionBar(toolbar);
+        ActionBar actionBar  =  getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
 
 
@@ -28,7 +30,7 @@ public class ThreadsActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.post_menu, menu);
         menu.getItem(0).getSubMenu().getItem(0).setChecked(true); // this grabs the recent menu option
 
         return true;
