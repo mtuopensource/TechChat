aa49f67c1a6e93d609ac15ac4d82354f102ad38a
Isaac Smith <isaacns98@gmail.com>
2019-02-23 15:52:25 -0500

    Reset web content

---
+++ backend/web/views/Helper.py
@@ -0,0 +1,62 @@
+import json
+from django.test import Client
+from api.response import SUCCESS
+from django.shortcuts import render, redirect
+
+def get_client(request):
+    if 'techchat_cookies' in request.session:
+        cookie = request.session['techchat_cookies'] # TODO Constants
+        return Client(HTTP_COOKIE=cookie)
+    return None
+
+def check_authentication(func):
+    def wrapper(*args, **kwargs):
+        client = get_client(next(x for x in args))
+        if client is not None:
+            return func(*args, **dict(kwargs, client=client))
+        return redirect('/web/login/')
+    return wrapper
+
+def get_board(client, id):
+    board = str(client.get(f'/api/boards/{id}/').content, 'utf-8')
+    return json.loads(board)
+
+def get_thread(client, id):
+    thread = str(client.get(f'/api/threads/{id}/').content, 'utf-8')
+    return json.loads(thread)
+
+def get_boards_list(client):
+    boards_list = str(client.get('/api/boards/').content, 'utf-8')
+    return json.loads(boards_list)
+
+def post_comment(client, id, comment_text):
+    response = client.post('/api/posts/', {
+        'thread':  id,
+        'content': comment_text
+    })
+    return response.status_code == 201
+
+def create_thread(client, board, thread_title, thread_content):
+    response = client.post('/api/threads/', {
+        'board': board,
+        'title': thread_title,
+        'content': thread_content
+    })
+    if response.status_code == 201:
+        return json.loads(response.content.decode('utf-8')).get('id')
+    return None
+
+def authenticate(request, username, password):
+    client = Client()
+    response = client.post('/api/users/login/', {
+        'email':    username,
+        'password': password
+    })
+    if response.status_code == SUCCESS.status_code:
+        request.session['techchat_cookies'] = client.cookies.output(header='', sep=';') # TODO Constants
+        request.session.modified = True
+        return redirect('/web/')
+    message = json.loads(str(response.content, 'utf-8'))
+    return render(request, 'login.html', {
+        'snackbar': message['detail']
+    })
