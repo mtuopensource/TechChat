a3f2e08672d8cffd4a0325e637e337a40e0c77d5
Ryan Philipps <raphilip@mtu.edu>
2019-03-11 16:32:18 -0400

    Created new Activity

---
+++ clients/Android/app/src/main/java/org/mtuosc/techchat/activity/CreatePostActivity.java
@@ -0,0 +1,13 @@
+package org.mtuosc.techchat.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class CreatePostActivity extends BaseInternetActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
